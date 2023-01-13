a128b8b8dff99ea66257628a20fcc594dc760063
Elisa <jc.alonsome@gmail.com>
2023-01-13 15:01:27 +0100

    start

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,104 +1,20 @@
-const pointsUserElement = document.getElementById('points-user');
-const pointsPcElement = document.getElementById('points-pc');
+const paper = document.getElementById('paper');
+const rock = document.getElementById('rock');
+const scissors = document.getElementById('scissors');
+const container = document.getElementById('container');
 
-const mainElement = document.getElementById('main');
+const movesOptions = ['paper', 'scissors', 'rock'];
 
-const firstStepElement = document.getElementById('first-step');
-const secondStepElement = document.getElementById('second-step');
+const pcMoves = () => {
+  let move = '';
 
-const gameResultElement = document.getElementById('game-result');
+  move =
+    move +
+    console.log(movesOptions[Math.floor(Math.random() * movesOptions.length)]);
+};
 
-const playAgainElement = document.getElementById('play-again');
-
-const GAME_OPTIONS = ['rock', 'scissors', 'paper'];
-
-const GAME_RULES = {
-  rock: {
-    scissors: true,
-    paper: false
-  },
-  scissors: {
-    rock: false,
-    paper: true
-  },
-  paper: {
-    scissors: false,
-    rock: true
+container.addEventListener('click', e => {
+  if (e.target.tagName === 'IMG' || e.target.classList.contains('game-item')) {
+    pcMoves();
   }
-};
-
-let currentScreen = 1;
-let pointsUser = 0;
-let pointsPc = 0;
-let userIcon;
-let pcIcon;
-
-const resetGame = () => {
-  userIcon.classList.remove('game-item--show');
-  pcIcon.classList.remove('game-item--show');
-};
-
-const changeScreen = () => {
-  if (currentScreen === 1) {
-    currentScreen = 2;
-    firstStepElement.classList.remove('first-step--show');
-    secondStepElement.classList.add('second-step--show');
-  } else {
-    currentScreen = 1;
-    firstStepElement.classList.add('first-step--show');
-    secondStepElement.classList.remove('second-step--show');
-  }
-};
-
-const updateScore = () => {
-  pointsPcElement.textContent = pointsPc;
-  pointsUserElement.textContent = pointsUser;
-};
-
-const checkWinner = (userPlay, pcPlay) => {
-  if (userPlay === undefined || pcPlay === undefined) {
-    console.log('ERROR');
-    return;
-  }
-
-  if (userPlay === pcPlay) {
-    gameResultElement.textContent = 'DRAW';
-    return;
-  }
-
-  if (GAME_RULES[userPlay][pcPlay]) {
-    gameResultElement.textContent = 'YOU WIN';
-    pointsUser++;
-  } else {
-    pointsPc++;
-    gameResultElement.textContent = 'YOU LOSE';
-  }
-
-  updateScore();
-};
-
-const optionsSelection = userSelection => {
-  const pcSelection = selectPcElement();
-  userIcon = document.getElementById(`${userSelection}-user`);
-  pcIcon = document.getElementById(`${pcSelection}-pc`);
-  userIcon.classList.add('game-item--show');
-  pcIcon.classList.add('game-item--show');
-  checkWinner(userSelection, pcSelection);
-};
-
-const selectPcElement = () => {
-  const result = Math.floor(Math.random() * (GAME_OPTIONS.length - 1));
-  return GAME_OPTIONS[result];
-};
-
-firstStepElement.addEventListener('click', ev => {
-  if (ev.target.classList.contains('game-item__image')) {
-    changeScreen();
-    optionsSelection(ev.target.dataset.element);
-  }
-});
-
-playAgainElement.addEventListener('click', () => {
-  changeScreen();
-  resetGame();
 });
